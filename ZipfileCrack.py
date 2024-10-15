c38b2ad521540cfe386bb478691363318549befc
Bilal-Anarwala <97702434+Bilal-Anarwala@users.noreply.github.com>
2024-10-14 23:29:18 -0500

    Add files via upload

---
+++ ZipfileCrack.py
@@ -0,0 +1,23 @@
+import zipfile
+from tqdm import tqdm
+
+wordlist = "rockyou.txt"
+# the zip file you want to crack its password
+zip_file = "JustStuff.zip"
+
+zip_file = zipfile.ZipFile(zip_file)
+# count the number of words in this wordlist
+n_words = len(list(open(wordlist, "rb")))
+# print the total number of passwords
+print("Total passwords to test:", n_words)
+
+with open(wordlist, "rb") as wordlist:
+    for word in tqdm(wordlist, total=n_words, unit="word"):
+        try:
+            zip_file.extractall(pwd=word.strip())
+        except:
+            continue
+        else:
+            print("[+] Password found:", word.decode().strip())
+            exit(0)
+print("[!] Password not found, try other wordlist.")
